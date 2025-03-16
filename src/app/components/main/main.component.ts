581aa5bc365adcd4b4c89e2bb46e444450a6a096
JaRodriguez97 <cristi542012@gmail.com>
2025-03-15 19:59:57 -0500

    Update modified files and add new assets

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -11,4 +11,34 @@ export class MainComponent {
   firstGrid = [1, 2, 3, 4, 5, 6];
   secondGrid = [10, 9, 8, 7, 6, 5];
   allGrids = [this.firstGrid, this.secondGrid];
+
+  loadedStatus: boolean[] = []; // Array para rastrear imágenes cargadas
+  totalImages: number;
+
+  constructor() {
+    this.totalImages = this.firstGrid.length + this.secondGrid.length;
+    this.loadedStatus = new Array(this.totalImages).fill(false);
+  }
+
+  onImageLoad(index: number) {
+    this.loadedStatus[index] = true;
+  }
+
+  shouldLoadImage(index: number): boolean {
+    return index === 0 || this.loadedStatus[index - 1];
+  }
 }
+
+/* 
+
+  constructor() {
+    // Inicializa el array de estado de carga con false
+    const totalImages = this.firstGrid.length + this.secondGrid.length;
+  }
+
+  onImageLoad(index: number) {
+  }
+
+  shouldLoadImage(index: number): boolean {
+    // Cargar la imagen si es la primera o si la anterior ya se cargó
+  } */
